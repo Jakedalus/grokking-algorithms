5a219fa5a2806d167bc24d8e06a5a11e47fd2e79
Jakedalus <jacquedalus@gmail.com>
2019-06-21 16:34:30 -0400

    Add random pivot version of quicksort

--- quickSort.js
+++ quickSort.js
@@ -1,4 +1,4 @@
-function quickSort(arr) {
+function basicQuickSort(arr) {
   if (arr.length < 2) {
     return arr;
   } else {
@@ -10,9 +10,50 @@ function quickSort(arr) {
       if (arr[i] > pivot) greater.push(arr[i]);
     }
     
+    return basicQuickSort(less).concat(pivot).concat(basicQuickSort(greater));
+  }
+}
+
+
+console.log('=================');
+console.log('basicQuickSort');
+console.log("[10, 5, 2, 3]:", basicQuickSort([10, 5, 2, 3]));
+console.log("[-10, 5, 2, 100, 33, -50, 1]:", basicQuickSort([-10, 5, 2, 100, 33, -50, 1]));
+console.log();
+
+
+function quickSort(arr) {
+  if (arr.length < 2) {
+    return arr;
+  } else {
+    var randomIndex = Math.floor(Math.random() * arr.length);
+    var pivot = arr[randomIndex];
+//    console.log(arr.length, randomIndex, pivot);
+    var less = [], greater = [];
+    
+    for(var i = 0; i < arr.length; i++) {
+      if (i !== randomIndex) {
+        if (arr[i] <= pivot) less.push(arr[i]);
+        if (arr[i] > pivot) greater.push(arr[i]);
+      }
+      
+    }
+    
     return quickSort(less).concat(pivot).concat(quickSort(greater));
   }
 }
 
+console.log('=================');
+console.log('quickSort');
 console.log("[10, 5, 2, 3]:", quickSort([10, 5, 2, 3]));
 console.log("[-10, 5, 2, 100, 33, -50, 1]:", quickSort([-10, 5, 2, 100, 33, -50, 1]));
+console.log("[-10, 5, 2, 100, 33, -50, 1, 0, 99, 23, 231, -1334]:", quickSort([-10, 5, 2, 100, 33, -50, 1, 0, 99, 23, 231, -1334]));
+
+
+
+
+
+
+
+
+
